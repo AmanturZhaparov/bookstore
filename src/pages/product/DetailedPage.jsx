627683a28d47043e25e95157fdf8f8b29f323c76
Turan Kutuev <turan.kutuev@onoisoftware.com>
2024-05-14 12:05:38 +0600

    fix: bookshop expande

--- src/pages/product/DetailedPage.jsx
+++ src/pages/product/DetailedPage.jsx
@@ -1,8 +1,8 @@
 import { useParams, useNavigate } from "react-router-dom";
 import PocketBase from "pocketbase";
 import React from "react";
-import "./k2d.css"
-import "./a.css"
+import "./k2d.css";
+import "./a.css";
 import { Flex, background } from "@chakra-ui/react";
 import ContainedButtons from "./button.jsx";
 import SimpleSnackbar from "./button.jsx";
@@ -14,7 +14,7 @@ const DetailedPage = () => {
 
   const getBook = async () => {
     const record = await pb.collection("Books").getOne(productId.id, {
-      //   expand: "relField1,relField2.subRelField",
+      expand: "Book_Shop",
       $autoCancel: false,
     });
     console.log(record);
@@ -25,48 +25,52 @@ const DetailedPage = () => {
     getBook();
   }, []);
   return (
-    <div> 
-      <img style={{
-        minWidth: 330,
-        borderRadius: 15,
-        marginTop: 90
-      }}
-      
-      
-         
+    <div>
+      <img
+        style={{
+          minWidth: 330,
+          borderRadius: 15,
+          marginTop: 90,
+        }}
         src={`https://book-shop.fly.dev/api/files/Books/${book?.id}/${book?.img}`}
         alt=""
       />
-      
-      
-      
-      <div style={{fontFamily: "K2D", textAlign: "left", fontWeight: 400, color: "black"}}>
-      
-      <h1 style={{fontSize:40, margin:0}}>{book?.title}</h1>
-      
-      <div style={{display: "flex", margin: 0,minWidth: 330}}>
-        <h1 style={{fontSize:30,color: "#7A8FFE"}}>Price</h1>
-        <h1  style={{fontSize:30}}>:{book?.price}$</h1>
+
+      <div
+        style={{
+          fontFamily: "K2D",
+          textAlign: "left",
+          fontWeight: 400,
+          color: "black",
+        }}
+      >
+        <h1 style={{ fontSize: 40, margin: 0 }}>{book?.title}</h1>
+        {book?.expand?.Book_Shop?.map((shop) => (
+          <p>
+            {shop.title} {shop.address}
+          </p>
+        ))}
+
+        <div style={{ display: "flex", margin: 0, minWidth: 330 }}>
+          <h1 style={{ fontSize: 30, color: "#7A8FFE" }}>Price</h1>
+          <h1 style={{ fontSize: 30 }}>:{book?.price}$</h1>
         </div>
-        <SimpleSnackbar/>
-        <div style={{display: "flex", margin: 0}}>
-        <h1 style={{fontSize:20,color: "#7A8FFE"}}>Author</h1>
-        <h1  style={{fontSize:20}}>:{book?.Author}</h1>
+        <SimpleSnackbar />
+        <div style={{ display: "flex", margin: 0 }}>
+          <h1 style={{ fontSize: 20, color: "#7A8FFE" }}>Author</h1>
+          <h1 style={{ fontSize: 20 }}>:{book?.Author}</h1>
         </div>
-        <h1  style={{fontSize:15,color:"black"}}>Where you can buy it:{book?.Book_Shop}</h1>
+        <h1 style={{ fontSize: 15, color: "black" }}>
+          Where you can buy it:{book?.Book_Shop}
+        </h1>
 
-
-      <div  className="as" dangerouslySetInnerHTML={{__html: book?.definition}}></div>
-
+        <div
+          className="as"
+          dangerouslySetInnerHTML={{ __html: book?.definition }}
+        ></div>
       </div>
-     
-     
     </div>
   );
 };
 
 export default DetailedPage;
-
-     
-         
-                 
